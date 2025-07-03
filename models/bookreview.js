998cfdc10c06cf4ed5bd402f8f42ce9957424cde
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2025-07-03 13:06:27 +0530

    Added publications

--- models/bookreview.js
+++ models/bookreview.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 let bookReviewSchema = mongoose.Schema({
     bookReview: String,
-    bookRating: String
+    bookRating: String,
+    bookPublications: String
 })
 
 module.exports = mongoose.model('bookReview', bookReviewSchema);
